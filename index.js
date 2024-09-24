1169371990ecc9279a63cad7ab9b7c9d41500f04
Mehdi Heydari <mmh287@cornell.edu>
2024-09-24 18:17:07 -0400

    added counter

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const WebSocket = require('ws');
 
 // Ensure the server is already running before starting the client
-const ws = new WebSocket('ws://localhost:8082');
+const ws = new WebSocket('ws://localhost:3000');
+
+let value = 0; // Initialize the counter outside of the setInterval
 
 ws.on('open', function open() {
   console.log('Connected to server');
   setInterval(() => {
-    const message = JSON.stringify({ x: 'Hello', y: 'World' });
+    const message = JSON.stringify({ x: 'Hello', y: 'World', counter: value });
     ws.send(message);
     console.log('Sent:', message);
-  }, 5000);
+    value++; // Increment the counter after sending the message
+  }, 1000);
 });
+
 
 ws.on('error', function error(err) {
   console.error('WebSocket Error:', err);
